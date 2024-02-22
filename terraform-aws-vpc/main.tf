5509ff67d96fe5af80d8732d485b6b304ea86b41
palakilaharikrishna <palakilaharikrishna@gmail.com>
2024-02-22 12:57:02 +0530

    [Feature] : AWS - Networking Modules  - Configured 2 - Tier Virtual Data Center BluePrint -- Publish into registry.terraform.io/modules/palakilaharikrishna

--- terraform-aws-vpc/main.tf
+++ terraform-aws-vpc/main.tf
@@ -1,12 +1,3 @@
-#Configure terraform version to working with list of providers like aws, azure , gcp, tfe, etc..
-# terraform latest version = "0.13.2"
-# provider -aws - latest version = "5.37.0"
-# Provider -azure - latest versoin = "3.90.0:
-# Provider -google - latest version = "5.17.0"
-# Provider -kubernetes latest version = "2.26.0"
-# Provider - helm- latest version = "1.1.1"
-
- 
 terraform {
     required_version = "=> 1.7.4"
 
@@ -38,81 +29,29 @@ provider "aws" {
     profile = var.profile-nvmumbai # apsouth1
 }
 
-provider "aws" {
-    version = "=> 5.37.0"
-    alias = var.useast2   # 
-    region = var.region-useast2   # us-east-2
-    profile = var.profile-useast2 # useastprofile
-}
 
-resource "aws_vpc" "jio-dev-vpc" {
-    cidr_block = "${var.cidr_block}"
-    instance_tenancy = "defulat"
-    tags = {
-        Name = "${var.vendor}-${var.client}-${var.domain}-${app-name}-${var.resource-type-vpc}"
-    }
-}
+module "aws-dbs-bank-dev-vpc" {
+    region                         = "${var.mregion}"
+    profile                        = "${var.mprofile}"
+    nvmumbai                       = "${var.mnvmumbai}"
+    region-nvmumbai                = "${var.mregion-nvmumbai}"
+    profile-nvmumbai               = "${var.mprofile-nvmumbai}"
+    useast2                        = "${var.museast2}"
+    region-useast2                 = "${var.mregion-useast2}"
+    profile-useast2                =  "${var.mregion-useast2}"
+    vendor                         = "${var.mvendor}"
+    client                         = "${var.mclient}"
+    domain                         = "${var.mdomain}"
+    env                            =  "${var.menv}"
+    app-name                       = "${var.mapp-name}"
+    resource-type-vpc              = "${var.mresource-type-vpc}"
+    resource-type-igw              =  "${var.mresource-type-igw}"
+    resource-type-pub-subnet       = "${var.mresource-type-subnet}"
+    resource-type-pub-rt           = "${var.mresource-type-pub-rt}"
+    resource-type-pvt-subnet       = "${var.mresource-type-pvt-subnet}"
+    resource-type-pvt-rt           = "${var.mresource-type-pvt-rg}"
+    cidr_block                     = "${var.mcidr_block}"
+    jio-pub-subnet-1-cidr_block    = "${var.mjio-pub-subnet-1-cidr_block}"
+    jio-pvt-subnet-1-cidr_block    =  "${var.mjio-pvt-subnet-1-cidr_block}"
 
-
-resource "aws_internet_gateway" "jio-dev-igw" {
-    vpc_id = aws-vpc.jio-dev-vpc.id
-    tags = {
-        Name = "${var.vendor}-${var.client}-${var.domain}-${app-name}-${var.resource-type-igw}"
-    }    
-
-}
-resource "aws_subnet" "jio-dev-pub-subnet-1"{
-    cidr_block = "${var.jio-pub-subnet-1-cidr_block}"
-    map_public_ip_on_launch = true 
-    vpc_id = aws-vpc.jio-dev-vpc.id
-    tags = {
-        Name = "${var.vendor}-${var.client}-${var.domain}-${app-name}-${var.resource-type-pub-subnet}"
-    }
-}
-
-resource "aws_route_table" "jio-dev-pub-rt" {
-    vpc_id = aws-vpc.jio-dev-vpc.id
-    rourte {
-        cidr_block = ["0.0.0.0/0"]
-        gateway_id = aws.aws_internet_gateway.jio-dev-igw.id
-    }
-    tags = {
-        Name = "${var.vendor}-${var.client}-${var.domain}-${app-name}-${var.resource-type-pub-rt}"
-    }
-}
-resource "aws_route_table_association" "jio-dev-rt-association" {
-    rourte_id = aws_route_table.jio-dev-pub-rt.id 
-    subnet_id = aws_subnet.jio-dev-pub-subnet-1
-}
-
-
-
-resource "aws_subnet" "jio-dev-pvt-subnet-1" {
-    vpc_id = aws_vpc.jio-dev-vpc.id
-    cidr_block = "${var.jio-pvt-subnet-1-cidr_block}
-    tags = {
-        Name = "${var.vendor}-${var.client}-${var.domain}-${app-name}-${var.resource-type-pvt-subnet}"
-    }
-}
-
-resource "aws_eip" "jio-dev-eip" {
-    vpc = true 
-}
-resource "aws_nat_gateway" "jio-dev-natgateway" {
-    associated_id = aws_eip.jio-dev-eip.id
-    subnet_id     = aws_subnet.jio-dev-pub-subnet-1.id
-}
-resource "aws_route_table" "jio-dev-pvt-rt-1" {
-    vpc_id = aws_vpc.jio-dev-vpc.id 
-    rourte {
-        cidr_block = ["0.0.0.0/0"]
-        nat_gatewate_id= aws_nat_gateway.jio-dev-natgateway.id 
-    }
-    tags = {
-        Name = "${var.vendor}-${var.client}-${var.domain}-${app-name}-${var.resource-type-pvt-rt}"
-    }
-}
-resource "aws_route_table_association" "jio-dev-pvt-rt-association" {
-    rourte_id = aws_route_table.jio-dev-pvt-rt-1.id
-    subnet_id = aws_subnet.jio-dev-pvt-subnet-1.id 
 }
